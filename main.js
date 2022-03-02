99c2b8612e4d6664bc7910812831a99c63ce4ec9
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-03-02 13:25:39 -0300

    Added registered email reply logic.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+/*confirmação de email do rodapé */
+function confirmaEmail() {
+  alert('Seu email foi cadastrado🎉🎉🎉');
+}
 
+document.querySelector('.rodape__formulario___botao').onclick = confirmaEmail;
