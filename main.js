ca43ec02edecd7ea595d3a60bad1dfc9390e859f
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-03-03 13:07:57 -0300

    Statically creating products.

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
-/*confirmação de email do rodapé */
+//cria os produtos de forma estatica
+const produtos = [
+  {
+    nome: 'Camiseta',
+    quantidade: 0
+  },
+  {
+    nome: 'Calça',
+    quantidade: 0
+  },
+  {
+    nome: 'Tenis',
+    quantidade: 0
+  },
+  {
+    nome: 'Boné',
+    quantidade: 0
+  },
+  {
+    nome: 'Liquidificador',
+    quantidade: 0
+  },
+  {
+    nome: 'Geladeira',
+    quantidade: 0
+  },
+  {
+    nome: 'Mixer',
+    quantidade: 0
+  },
+  {
+    nome: 'Microondas',
+    quantidade: 0
+  }
+]
+
+//confirmação de email do rodapé
 function confirmaEmail() {
   alert('Seu email foi cadastrado🎉🎉🎉');
 }
